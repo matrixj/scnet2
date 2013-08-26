a4bde464a368d8d1b13a88cc67d1ff1decef0136
chenjieqiang <chenjieqiang@gmail.com>
2013-08-26 09:51:53 +0800

    Use google cpplint.py to check coding style :)

--- src/net/epoller.h
+++ src/net/epoller.h
@@ -1,21 +1,17 @@
-#ifndef SCNET2_NET_EPOLLER_H
-#define SCNET2_NET_EPOLLER_H
-
-#include <vector>
-#include <map>
+#ifndef SRC_NET_EPOLLER_H_
+#define SRC_NET_EPOLLER_H_
 
 #include <poll.h>
 #include <sys/epoll.h>
+
+#include <vector>
+#include <map>
 
 #include <net/poller.h>
 #include <base/timestamp.h>
 
 namespace scnet2 {
 
-//namespace base{
-//class BaseLoop;
-//class Channel;
-//}
 class BaseLoop;
 class Channel;
 
@@ -23,18 +19,18 @@ namespace net {
 
 class Epoller : public Poller {
  public:
-  Epoller(BaseLoop *loop);
+  explicit Epoller(BaseLoop *loop);
   virtual ~Epoller();
 
   virtual Timestamp wait(int timeout, std::vector<Channel*> *channels);
   virtual void updateChannel(Channel *c);
 
  private:
-  //size is ignored nowaday in epoll_create(1)
+  // Size is ignored nowaday in epoll_create(1)
   const static int SIZE = 11;
   BaseLoop *_loop;
   int _epollfd;
-  //int _timeout;
+  // int _timeout;
 
   std::vector<struct epoll_event> _events;
   typedef std::map<int, Channel*> ChannelMap;
@@ -43,11 +39,9 @@ class Epoller : public Poller {
 
   void padChannels(int number, std::vector<Channel*> *channel);
   void update(int operation, Channel *c);
-
 };
 
+}  // namespace net
+}  // namespace scnet2
 
-}
-}
-
-#endif
+#endif  // SRC_NET_EPOLLER_H_
